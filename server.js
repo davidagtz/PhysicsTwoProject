0c0b91a1a4aec640574b2c16fe0df4d78ddc206e
David Gutierrez <david.a.gutierrez99@gmail.com>
2018-05-16 12:39:28 -0500

    Implemented Sass

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var pug = require('pug');
+var sass = require('sass');
+var fs = require('fs');
 
 var app = express();
 
@@ -7,10 +9,17 @@ app.use(express.static("www"));
 
 var index = pug.compileFile("templates/index.pug");
 
+sass2css("www/css/main.css", sass.renderSync({
+    file: "sass/main.sass"
+}));
+
+
 app.get("/", (req, res) => {
-    res.send(index({
-        name: "Jakob"
-    }));
+    res.send(index({}));
 });
 
 app.listen(80);
+
+function sass2css(path, res) {
+    fs.writeFileSync(path, res.css.toString());
+}
