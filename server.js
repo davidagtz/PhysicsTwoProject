bd9fd3cd6e41dbaf1abb2f1b4c3dcd4ccc31e15e
David Gutierrez <david.a.gutierrez99@gmail.com>
2018-05-20 16:26:58 -0500

    fixed heroku error

--- server.js
+++ server.js
@@ -75,9 +75,9 @@ app.get("*", (req, res) => {
     }
 });
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8080;
 app.listen(port, ()=>{
-    console.log("Server is running on "+port);
+    console.log("Server is running on " + port);
 });
 
 function sass2css(path, res) {
